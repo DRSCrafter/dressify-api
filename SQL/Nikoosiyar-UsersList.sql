6aa21242a01cb9ba41b878fdccd95395eba4b5b8
bahare nikoosiyar <bahare.nikoosiyar@gmail.com>
2023-01-05 17:10:50 +0330

    insert user list

---
+++ SQL/Nikoosiyar-UsersList.sql
@@ -0,0 +1,48 @@
+insert into user values (default, "امیر محمد", "محمدزاده", 0, "amirMohammad.mohammadzadeh@gmail.com", "1234", curdate());
+insert into useraddress values ("شیراز", "پاسداران", 1);
+insert into useraddress values ("َشیراز", "ستارخان", 1);
+insert into userphonenumbers values("09352884424", 1);
+
+insert into user values (default, "معصومه", "کشاورز", 0, "masoume.keshavarz@gmail.com", "masoume234*", curdate());
+insert into useraddress values ("مشهد", "احمدآباد", 2);
+insert into useraddress values ("مشهد", "وکیل آباد", 2);
+insert into userphonenumbers values("09035129832", 2);
+
+insert into user values (default, "سینا", "زارعی", 0, "sina34z@gmail.com", "qweert34!#", curdate());
+insert into useraddress values ("تهران", "انقلاب", 3);
+insert into userphonenumbers values("09158730187",3);
+
+insert into user values (default, "گلی", "شفیعی", 0, "goligoli1376@gmail.com", "zxjsjdfAA12", curdate());
+insert into useraddress values ("اضفهان", "عباس آباد",4);
+insert into useraddress values ("اضفهان", "هشت بهشت",4);
+insert into userphonenumbers values("09151073287",4);
+
+insert into user values (default, "علی", "ظهوریان", 0, "ali2128@gmail.com", "324324Ak", curdate());
+insert into useraddress values ("مشهد", "سحاد",5);
+insert into userphonenumbers values("09127629843",5);
+
+insert into user values (default, "سحر", "حسینی", 0, "saharHosseini@gmail.com", "reZZZZaYu", curdate());
+insert into useraddress values ("تهران", "صادقیه",6);
+insert into useraddress values ("تهران", "محمدیه",6);
+insert into userphonenumbers values("09127629843",6);
+
+insert into user values (default, "غلام", "امینی", 0, "gholam432gmail.com", "qwertGHolam222", curdate());
+insert into useraddress values ("ِیزد", "امام علی",7);
+insert into userphonenumbers values("09037184378",7);
+
+insert into user values (default, "مژده", "صادقی", 0, "Mozhde_Sadeghi@gmail.com", "mozhi34322__3", curdate());
+insert into useraddress values ("مشهد", "دانشجو", 8);
+insert into userphonenumbers values("09128732984",8);
+
+insert into user values (default, "شهریار", "زارع پور", 0, "shahriyaaaar1379@gmail.com", "92672187423AAA", curdate());
+insert into useraddress values ("تهران", "فرشته", 9);
+insert into userphonenumbers values("09036719810",9);
+
+insert into user values (default, "سارا", "نقی زاده", 0, "SaraNaghizade12@gmail.com", "98sara13808080", curdate());
+insert into useraddress values ("مشهد", "فلسطین", 10);
+insert into userphonenumbers values("09035187610",10);
+
+insert into user values (default, "آرزو", "محمدی", 0, "Arezooooo_m75@gmail.com", "a1298***6", curdate());
+insert into useraddress values ("مشهد", "مجیدیه", 11);
+insert into userphonenumbers values("09051209821",11);
+
