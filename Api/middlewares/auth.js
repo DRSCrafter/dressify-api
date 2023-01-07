a9a48e306e1f22ea03c069f38179aebd5fff101d
Mohammad Reza Arasteh <drsprogramming2020@gmail.com>
2023-01-07 13:12:02 +0330

    Add Admin check middleware

--- Api/middlewares/auth.js
+++ Api/middlewares/auth.js
@@ -2,6 +2,9 @@ import connection from "../utils/db.js";
 import users from '../utils/users.js';
 
 export default (req, res, next) => {
+    if (!req.header("x-auth-email"))
+        return res.status(400).send("Authorization headers are ");
+
     connection.query(
         `select user_id from user where email = "${req.header("x-auth-email")}"`,
         (err, results) => {
@@ -9,7 +12,7 @@ export default (req, res, next) => {
             const userID = results[0].user_id;
             const exists = users.find(id => userID == id);
             if (!exists)
-                return res.status(400).send("User is not logged in!");
+                return res.status(403).send("User is not logged in!");
             else
                 next();
         }
