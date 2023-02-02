860464fb3118e0642449764a196a9a0d5f2a8d02
Mohammad Reza Arasteh <drsprogramming2020@gmail.com>
2023-02-02 18:37:57 +0330

    Add a couple of missing controllers regarding user signup and edit

--- Api/controllers/userController.js
+++ Api/controllers/userController.js
@@ -24,10 +24,10 @@ const controllers = {
         // order by totalBuy desc;
         db.query(
             'select user_id, sum(cost) as totalBuy ' +
-                'from user, cart, payment ' +
-                'where user_id = User_user_id and cart_id = Cart_cart_id and year(payment.date_paid) = year(now()) and week(payment.date_paid) = week(now()) ' +
-                'group by user_id ' +
-                'order by totalBuy desc;',
+            'from user, cart, payment ' +
+            'where user_id = User_user_id and cart_id = Cart_cart_id and year(payment.date_paid) = year(now()) and week(payment.date_paid) = week(now()) ' +
+            'group by user_id ' +
+            'order by totalBuy desc;',
             (err, results) => {
                 if (err) return console.log(err);
                 res.send(results);
@@ -43,10 +43,10 @@ const controllers = {
         // order by totalBuy desc;
         db.query(
             'select user_id, sum(cost) as totalBuy ' +
-                'from user, cart, payment ' +
-                'where user_id = User_user_id and cart_id = Cart_cart_id and year(payment.date_paid) = year(now()) and month(payment.date_paid) = month(now()) ' +
-                'group by user_id ' +
-                'order by totalBuy desc;',
+            'from user, cart, payment ' +
+            'where user_id = User_user_id and cart_id = Cart_cart_id and year(payment.date_paid) = year(now()) and month(payment.date_paid) = month(now()) ' +
+            'group by user_id ' +
+            'order by totalBuy desc;',
             (err, results) => {
                 if (err) return console.log(err);
                 res.send(results);
@@ -62,7 +62,7 @@ const controllers = {
             `select o.order_id, p.name, o.quantity, o.total_cost, o.DiscountCode_discount_code_id 
                 from user u, dbproject.order o, product p 
                 where u.user_id = o.User_user_id and o.Product_product_id = p.product_id 
-                and u.user_id = ${req.params.userID};`,
+                and u.email = "${req.header["x-auth-email"]}";`,
             (err, results) => {
                 if (err) return console.log(err);
                 res.send(results);
@@ -84,7 +84,7 @@ const controllers = {
                  on o.Cart_cart_id = c.cart_id 
                  join payment p 
                  on c.cart_id = p.Cart_cart_id 
-                 where u.user_id = "${req.params.userID}" 
+                 where u.email = "${req.header["x-auth-email"]}" 
                  order by date_paid desc 
                  limit 10;`,
             (err, results) => {
@@ -120,7 +120,7 @@ const controllers = {
                     `insert into dbproject.user
                      values (default, "${req.body.first_name}", "${req.body.last_name}", ${req.body.isAdmin},
                              "${req.body.email}", "${password}", curdate());`,
-                    (err) => {
+                    (err, results) => {
                         if (err) return console.log(err);
                     }
                 );
@@ -131,7 +131,34 @@ const controllers = {
                      where email = "${req.body.email}";`,
                     (err, results) => {
                         if (err) return console.log(err);
-                        users.push(results[0].user_id);
+                        const userID = results[0].user_id;
+                        users.push(userID);
+
+                        let addresses = `insert into dbproject.useraddress values`;
+                        for (let address of req.body.addresses)
+                            addresses += `("${address.city}", "${address.street}", ${userID}),`
+                        let index = addresses.lastIndexOf(',');
+                        addresses = addresses.slice(0, index);
+                        addresses += ';';
+                        admin.query(
+                            addresses,
+                            (err) => {
+                                if (err) return console.log(err);
+                            }
+                        );
+
+                        let phoneNumbers = `insert into dbproject.userphonenumbers values`;
+                        for (let phoneNumber of req.body.phoneNumbers)
+                            phoneNumbers += `("${phoneNumber}", ${userID}),`
+                        index = phoneNumbers.lastIndexOf(',');
+                        phoneNumbers = phoneNumbers.slice(0, index);
+                        phoneNumbers += ';';
+                        admin.query(
+                            phoneNumbers,
+                            (err) => {
+                                if (err) return console.log(err);
+                            }
+                        );
                     }
                 );
                 res.send("Done");
@@ -234,6 +261,56 @@ const controllers = {
         )
 
     }, // 19 21
+    postAddress: (req, res) => {
+        db.query(
+            `select user_id
+                 from dbproject.user
+                 where email = "${req.header['x-auth-email']}";`,
+            (err, results) => {
+                if (err) return console.log(err);
+                const userID = results[0].user_id;
+
+                let addresses = `insert into dbproject.useraddress values`;
+                for (let address of req.body.addresses)
+                    addresses += `("${address.city}", "${address.street}", ${userID}),`
+                const index = addresses.lastIndexOf(',');
+                addresses = addresses.slice(0, index);
+                addresses += ';';
+                admin.query(
+                    addresses,
+                    (err) => {
+                        if (err) return console.log(err);
+                    }
+                );
+            }
+        );
+        res.send("Done");
+    },
+    postPhoneNumber: (req, res) => {
+        db.query(
+            `select user_id
+                 from dbproject.user
+                 where email = "${req.header['x-auth-email']}";`,
+            (err, results) => {
+                if (err) return console.log(err);
+                const userID = results[0].user_id;
+
+                let phoneNumbers = `insert into dbproject.userphonenumbers values`;
+                for (let phoneNumber of req.body.phoneNumbers)
+                    phoneNumbers += `("${phoneNumber}", ${userID}),`
+                const index = phoneNumbers.lastIndexOf(',');
+                phoneNumbers = phoneNumbers.slice(0, index);
+                phoneNumbers += ';';
+                admin.query(
+                    phoneNumbers,
+                    (err) => {
+                        if (err) return console.log(err);
+                    }
+                );
+            }
+        );
+        res.send("Done");
+    },
     deleteUser: (req, res) => {
         controllers.logout(req, res);
         admin.query(
@@ -241,8 +318,47 @@ const controllers = {
             (err) => {
                 if (err) return res.send(err);
             }
-        )
+        );
+        res.send("Done");
     }, // 19
+    deletePhoneNumber: (req, res) => {
+        db.query(
+            `select user_id
+                 from dbproject.user
+                 where email = "${req.header['x-auth-email']}";`,
+            (err, results) => {
+                if (err) return console.log(err);
+                const userID = results[0].user_id;
+                admin.query(
+                    `delete from dbproject.userphonenumbers
+                         where User_user_id = ${userID} and phone_number = "${req.body.phoneNumber}"`,
+                    (err) => {
+                        if (err) return console.log(err);
+                    }
+                );
+            }
+        );
+        res.send("Done");
+    },
+    deleteAddress: (req, res) => {
+        db.query(
+            `select user_id
+                 from dbproject.user
+                 where email = "${req.header['x-auth-email']}";`,
+            (err, results) => {
+                if (err) return console.log(err);
+                const userID = results[0].user_id;
+                admin.query(
+                    `delete from dbproject.useraddress
+                         where User_user_id = ${userID} and city = "${req.body.city}" and street = "${req.body.street}"`,
+                    (err) => {
+                        if (err) return console.log(err);
+                    }
+                );
+            }
+        );
+        res.send("Done");
+    }
 }
 
 
--- Api/routes/users.js
+++ Api/routes/users.js
@@ -5,15 +5,19 @@ import auth from "../middlewares/auth.js";
 const router = express.Router();
 
 router.get('/', auth, userController.getUsers);
-router.get('/:userID/orders', auth, userController.getOrders);
-router.get('/:userID/latestorders', auth, userController.getLatestOrders);
+router.get('/orders', auth, userController.getOrders);
+router.get('/latestorders', auth, userController.getLatestOrders);
 router.get('/city/:city', userController.getUsersInCity);
 router.get('/topweek', auth, userController.getTopWeek);
 router.get('/topmonth', auth, userController.getTopMonth);
 router.post('/', userController.signUp);
 router.post('/login', userController.login);
 router.post('/logout', auth, userController.logout);
+router.post('/address', auth, userController.postAddress);
+router.post('/phonenumber', auth, userController.postPhoneNumber);
 router.put('/', auth, userController.editUser);
 router.delete('/', auth, userController.deleteUser);
+router.delete('/address', auth, userController.deleteAddress);
+router.delete('/phonenumber', auth, userController.deletePhoneNumber);
 
 export default router;
